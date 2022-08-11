523e3b99fac2ad19737698ba8fe36cd8bffaac91
Jason Xu <40355221+JasonBrave@users.noreply.github.com>
2022-08-11 16:35:05 -0400

    Remove deprecated WebSecurityConfigurerAdapter

--- src/main/java/com/projectory/projectory/WebSecurityConfig.java
+++ src/main/java/com/projectory/projectory/WebSecurityConfig.java
@@ -4,22 +4,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+public class WebSecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.cors().disable();
-		http.addFilterBefore(new UsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+		http.addFilterAt(new UsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
 				.antMatchers(HttpMethod.GET, "/server_version").permitAll()
 				.antMatchers(HttpMethod.POST, "/auth/login", "/auth/register").permitAll()
@@ -31,16 +28,17 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.successHandler(new LoginSuccessHandler()).failureHandler(new LoginFailtureHandler())
 				.and()
 				.logout().logoutUrl("/auth/logout").permitAll();
+		return http.build();
 	}
 
 	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
+	public InMemoryUserDetailsManager userDetailService() {
 		UserDetails user = User.withDefaultPasswordEncoder()
-				.username("u")
-				.password("p")
+				.username("user")
+				.password("password")
 				.roles("USER")
 				.build();
 		return new InMemoryUserDetailsManager(user);
 	}
+
 }
